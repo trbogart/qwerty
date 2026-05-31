8776922c9e538b5d2f433cfd7bcc843e353099ec
Thomas Bogart <trbogart@gmail.com>
2026-05-31 16:11:16 -0700

    Simplify mask counts

--- querty8_multi.py
+++ querty8_multi.py
@@ -4,29 +4,21 @@ from typing import Iterator, Tuple
 from common import get_all_words, get_finger_mask
 
 
-class Words:
-    def __init__(self, mask: int):
-        self.mask = mask
-        self.count = 0
-
-    def add_word(self, word: str) -> None:
-        self.count += 1
-
-
 class Qwerty:
     def __init__(self):
-        self.mask_words: list[Words] = [Words(i) for i in range(256)]
+        self.mask_counts = [0] * 256
         for word in get_all_words():
             mask = get_finger_mask(word)
             if mask.bit_count() == len(word):
-                self.mask_words[mask].add_word(word)
+                self.mask_counts[mask] += 1
 
     def run(self):
         counts = [self.get_count(max_words=max_words) for max_words in range(9)]
         for num_words in range(1, len(counts)):
             count = counts[num_words] - counts[num_words - 1]
             if count > 0:
-                print(f'{count:,} groups of {num_words} words')
+                print(f'{num_words} word{'s: ' if num_words > 1 else ':  '}{count:-7,d}')
+        print(f'Total:   {counts[-1]:-7,d}')
 
     def get_count(self, max_words: int):
         if max_words == 0:
@@ -38,14 +30,13 @@ class Qwerty:
         if mask_counts[mask] >= 0:
             return mask_counts[mask]
 
-        count = self.mask_words[mask].count
+        count = self.mask_counts[mask]
         if num_words < max_words:
             for mask1, mask2 in self._get_sub_masks(mask):
                 count1 = self._get_count(mask1, mask_counts, max_words, num_words + 1)
                 if count1 > 0:
                     count2 = self._get_count(mask2, mask_counts, max_words, num_words + 1)
                     count += count1 * count2
-
         mask_counts[mask] = count
         return count
 
